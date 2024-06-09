46c2d0bc6529d93ebc73b990779e0373aebe4d76
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2024-06-09 16:35:08 -0300

    Delete pacote directory

--- pacote/ext/conversorExcel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-# Exportar banco de dados como .xlsx
-def converter_para_excel():
-    engine = create_engine('sqlite:///cadastro.db')
-    df = pd.read_sql_table('usuario', engine)
-    df.to_excel('usuarios.xlsx', index=False)
-
